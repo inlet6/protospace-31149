b40cbe16ade302db12e3ddba3e1ceaaf9756ae32
inlet6 <72550990+inlet6@users.noreply.github.com>
2020-10-30 12:43:49 +0900

    プロトタイプの投稿機能　仮実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,6 @@
 class PrototypesController < ApplicationController
   def index
+    @prototype = Prototype.all
   end
 
   def new
@@ -7,5 +8,17 @@ class PrototypesController < ApplicationController
   end
 
   def create
+    Prototype.create(prototype_params)
+    if current_user.created_at
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title,:catch_copy,:concept,:image).merge(user_id: current_user.id)
+  end
+
 end
